42c4410e98d774a06f0f785cba63f5bc92d965d5
Marat Maksumov <maksumov@users.noreply.github.com>
2021-04-03 22:02:02 +0300

    refactor: replace img tag selector according to rule 3.4 in html-and-css.md
    resolves #13

--- app.js
+++ app.js
@@ -48,6 +48,7 @@ var createNewTaskElement=function(taskString){
 
   deleteButton.className="button delete";
   deleteButtonImg.src="./remove.svg";
+  deleteButtonImg.className="remove-icon";
   deleteButton.appendChild(deleteButtonImg);
 
 
