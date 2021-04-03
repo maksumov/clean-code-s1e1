175b7cdc3004e7682d340cc3ed940463580663c3
Marat Maksumov <maksumov@users.noreply.github.com>
2021-04-03 21:27:49 +0300

    refactor: replace tag selectors according to rule 3.4 in html-and-css.md
    resolves #13

--- app.js
+++ app.js
@@ -32,18 +32,21 @@ var createNewTaskElement=function(taskString){
   var deleteButton=document.createElement("button");//delete button
   var deleteButtonImg=document.createElement("img");//delete button image
 
+  listItem.className="item";
+
   label.innerText=taskString;
-  label.className="task";
+  label.className="label task";
 
   //Each elements, needs appending
   checkBox.type="checkbox";
+  checkBox.className="input";
   editInput.type="text";
-  editInput.className="task";
+  editInput.className="input task";
 
   editButton.innerText="Edit"; //innerText encodes special characters, HTML does not.
-  editButton.className="edit";
+  editButton.className="button edit";
 
-  deleteButton.className="delete";
+  deleteButton.className="button delete";
   deleteButtonImg.src="./remove.svg";
   deleteButton.appendChild(deleteButtonImg);
 
